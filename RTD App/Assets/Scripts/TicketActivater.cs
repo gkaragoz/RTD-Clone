7a79ead20646054cdec40f7a078fb71ff30f2b72
gkaragoz <annotto3@gmail.com>
2018-08-22 16:43:10 -0600

    Bug fixes on meridiem shows.

--- RTD App/Assets/Scripts/TicketActivater.cs
+++ RTD App/Assets/Scripts/TicketActivater.cs
@@ -12,6 +12,16 @@ public class TicketActivater : MonoBehaviour {
 
 	void Start () {
         DateTime dt1 = DateTime.Now.AddHours(-1).AddMinutes(UnityEngine.Random.Range(1, 50));
-        txtTicketActivated.text = TICKET_ACTIVATED_MESSAGE + dt1.ToShortTimeString();
-	}
+        string parsedTime = DateTime.Parse(dt1.ToLongTimeString()).ToString("h:mm tt");
+        txtTicketActivated.text = TICKET_ACTIVATED_MESSAGE + parsedTime;
+
+        /*
+        DateTime dt = DateTime.Parse("6/22/2009 07:00:00 AM");
+
+        dt.ToString("HH:mm"); // 07:00 // 24 hour clock // hour is always 2 digits
+        dt.ToString("hh:mm tt"); // 07:00 AM // 12 hour clock // hour is always 2 digits
+        dt.ToString("H:mm"); // 7:00 // 24 hour clock
+        dt.ToString("h:mm tt"); // 7:00 AM // 12 hour clock
+        */
+    }
 }
