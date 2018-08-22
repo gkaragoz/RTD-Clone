5561332710b8d610353dc6949fae7e4b35a23712
gkaragoz <annotto3@gmail.com>
2018-08-22 14:52:22 -0600

    Create DateToText script and finish v0.1

---
+++ RTD App/Assets/Scripts/DateToText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateToText : MonoBehaviour {
+
+    public Text txtDate;
+    public Text txtClock;
+
+	void Update () {
+        string shortDateString = System.DateTime.Now.ToShortDateString();
+        string year = shortDateString.Substring(shortDateString.Length - 2);
+        shortDateString = shortDateString.Replace(shortDateString, shortDateString.Substring(0, shortDateString.Length - 4) + year);
+        txtDate.text = shortDateString;
+
+        string longTimeString = System.DateTime.Now.ToLongTimeString();
+        txtClock.text = longTimeString;
+	}
+}
